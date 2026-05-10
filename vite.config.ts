25aee828c40abc23019fcf00ee06b69b6b796c65
agustinlopez9 <fernandopiceda99@gmail.com>
2026-05-10 16:37:51 -0300

    Install eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint .',
+        useFlatConfig: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
